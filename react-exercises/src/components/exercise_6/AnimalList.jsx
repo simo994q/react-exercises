71580dfc24874165086bcfb99e4cfbfce945efa8
simo994q <simonkn1@hotmail.com>
2023-08-24 20:42:33 +0200

    exercises 6

--- react-exercises/src/components/exercise_6/AnimalList.jsx
+++ react-exercises/src/components/exercise_6/AnimalList.jsx
@@ -1,5 +1,16 @@
+import style from './Exercise_6.module.scss'
 
-
-export function AnimalList () {
-    
+export function AnimalList(props) {
+    return (
+        <div>
+            {props.list.map((item, index) => {
+                return (
+                    <div className={style.itemContainer} key={index}>
+                        <img src={item.icon} alt="" />
+                        <p>{item.name}</p>
+                    </div>
+            )
+            })}
+        </div>
+    )
 }
--- react-exercises/src/components/exercise_6/Exercise_6.jsx
+++ react-exercises/src/components/exercise_6/Exercise_6.jsx
@@ -1,9 +1,25 @@
 import style from './Exercise_6.module.scss'
 import { AnimalList } from './AnimalList.jsx'
 
+import cat from './assets/cat.svg'
+import chicken from './assets/chicken.svg'
+import cow from './assets/cow.svg'
+import dog from './assets/dog.svg'
+import horse from './assets/horse.svg'
+import sheep from './assets/sheep.svg'
+
 export function Exercise_6 () {
 
+    ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse']
 
+    const animals = [
+        {name: 'cat', icon: cat},
+        {name: 'chicken', icon: chicken},
+        {name: 'cow', icon: cow},
+        {name: 'dog', icon: dog},
+        {name: 'horse', icon: horse},
+        {name: 'sheep', icon: sheep}
+    ]
 
     return (
         <div className={style.container}>
